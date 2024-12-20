cfa265edc1b61036a3dc46cc32d5f4eff034011d
zombiedestroyer29 <rollsgavin10@gmail.com>
2024-12-20 16:09:22 -0600

    Add PC GfxWorldDraw

---
+++ GfxWorld.h
@@ -0,0 +1,22 @@
+struct GfxWorldDraw
+{
+  unsigned int reflectionProbeCount;
+  $FB38F030EA0083BD0D17EC888129F137 ___u1;
+  $B4EF3D38BE8610BBA4EBD5354A969379 ___u2;
+  int lightmapCount;
+  $635E812FAB080B9B3A905987B98AC8CD ___u4;
+  $8C0CD2F20D4AB90CEF4B7EF1776CD269 ___u5;
+  $40E81F39AA7EA8DA6EF0A198775F1226 ___u6;
+  unsigned int vertexCount;
+  unsigned int vertexDataSize0;
+  GfxWorldVertexData0 vd0;
+  unsigned int vertexDataSize1;
+  GfxWorldVertexData1 vd1;
+  int indexCount;
+  unsigned __int16 *indices;
+  void *indexBuffer;
+};
+
+
+
+struct GfxWorld; //seemingly same as pc
